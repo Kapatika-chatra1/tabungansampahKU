7f2d71be9d58575aef5785720308749a22610d77
mochamaulXakbar <maulanamochamadakbar@gmail.com>
2025-09-01 00:30:23 +0700

    Tambah file user.css dan update user.php

--- login.js
+++ login.js
@@ -8,10 +8,19 @@ function showForm(formType) {
     registerForm.classList.remove("active");
     tabs[0].classList.add("active");
     tabs[1].classList.remove("active");
+    document.getElementById("no_hp").focus(); // auto focus
   } else {
     registerForm.classList.add("active");
     loginForm.classList.remove("active");
     tabs[1].classList.add("active");
     tabs[0].classList.remove("active");
+    document.querySelector("input[name='nama']").focus(); // auto focus
+  }
+}
+
+function togglePassword(id) {
+  const input = document.getElementById(id);
+  if (input) {
+    input.type = input.type === "password" ? "text" : "password";
   }
 }
